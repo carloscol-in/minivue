fe42334d0e6997d42902ca498488d647eb118305
Carlos <contact@carloscolin.me>
2021-08-03 14:13:11 -0500

    Using Reflect

--- pew.js
+++ pew.js
@@ -5,13 +5,16 @@ class PewReactive {
         // destiny
         this.$data = new Proxy(this.origin, {
             get(target, name) {
-                if ( name in target ) {
-                    return target[name];
+                if ( Reflect.has(target, name) ) {
+                    return Reflect.get(target, name);
                 }
 
                 console.warn(`Property "${name}" does not exist on object.`);
                 return "";
             },
+            set(target, name, value) {
+                Reflect.set(target, name, value);
+            }
         });
     }
 
@@ -19,13 +22,24 @@ class PewReactive {
         document.querySelectorAll("*[p-text]").forEach(el => {
             this.pText(el, this.$data, el.getAttribute("p-text"));
         });
+
+        document.querySelectorAll("*[p-model]").forEach(el => {
+            const name = el.getAttribute("p-model");
+            this.pModel(el, this.$data, name);
+
+            el.addEventListener("input", () => {
+                Reflect.set(this.$data, name, el.value)
+            });
+        })
     }
 
     pText(el, target, name) {
-        el.innerText = target[name];
+        el.innerText = Reflect.get(target, name);
     }
 
-    pModel() {}
+    pModel(el, target, name) {
+        el.value = Reflect.get(target, name);
+    }
 }
 
 var Pew = {
